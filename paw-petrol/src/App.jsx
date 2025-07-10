fa28c38f32aff769cd4036beb928457761b0d2ec
Cwd2003 <Deepak20032004tiwari@gmail.com>
2025-07-10 22:11:33 +0530

    ...

--- paw-petrol/src/App.jsx
+++ paw-petrol/src/App.jsx
@@ -3,13 +3,19 @@ import About from "./components/pages/About"
 import Blog from "./components/pages/Blog"
 import Services from "./components/pages/Services"
 import Vet from "./components/pages/Vet"
+import Blogsingle from "./components/pages/Blogsingle"
+import Gallery from "./components/pages/Gallery"
+import Pricing from "./components/pages/Pricing"
 function App(){
   return (
    // <About/>
    // <Contact/>
    // <Blog></Blog>
     //<Services/>
-    <Vet/>
-    )
+    //<Vet/>
+  //<Blogsingle/> 
+  <Gallery/>
+//<Pricing/>  
+)
 }
 export default App
